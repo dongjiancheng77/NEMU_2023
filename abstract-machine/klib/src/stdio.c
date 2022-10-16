614932627325ab9b7fe9b85c643961f5db1404f6
tracer-ics2022 <tracer@njuics.org>
2022-10-16 16:59:04 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:59:04 up 48 min,  1 user,  load average: 0.61, 0.42, 0.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,6 +8,9 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 #define BUF_LEN 256
+
+// itoa()函数把整数转换成字符串，并返回指向转换后的字符串的指针。
+
 char *itoa(int value, char *str, int radix)
 {
   char reverse[36];
@@ -40,16 +43,37 @@ char *itoa(int value, char *str, int radix)
 
   return str;
 }
+char *uitoa(uint32_t value, char *str, int radix)
+{
+  char reverse[36];
+  char *p = reverse;
 
+  *p++ = '\0';
+  while (value != 0)
+  {
+    *p++ = "0123456789abcdef"[value % radix];
+    value /= radix;
+    if (value == 0)
+      break;
+  }
+  p--;
+
+  while (p >= reverse)
+  {
+    *str++ = *p--;
+  }
+
+  return str;
+}
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
 {
   char buffer[128];
-char *str;
+  char *str;
   int val, len;
   unsigned int unum;
   uint32_t pointer;
   bool state = true;
-  int i = 0, j = 0;
+  int i = 0, j = 0, k = 0;
   while (fmt[i] != '\0' && j + 1 < n)
   {
     if (state)
@@ -73,7 +97,7 @@ char *str;
         break;
       case 's':
         str = va_arg(ap, char *);
-        int k = 0;
+        k = 0;
         while (str[k] != '\0')
         {
           out[j++] = str[k++];
@@ -97,9 +121,9 @@ char *str;
         for (int k = len - 1; k >= 0; --k)
           out[j++] = buffer[k];
 
-          // itoa(val,str,10);
-          // for (; val; val /= 10)
-          // j++;
+        // itoa(val,str,10);
+        // for (; val; val /= 10)
+        // j++;
         break;
 
       case 'p':
@@ -115,16 +139,22 @@ char *str;
 
       case 'x':
         unum = va_arg(ap, unsigned int);
-        if (unum == 0)
-        {
-          out[j++] = '0';
-          break;
-        }
-        for (len = 0; unum; unum >>= 4, ++len)
-          buffer[len] = "0123456789ABCDEF"[unum & 0xF]; //逆序的
+        // if (unum == 0)
+        // {
+        //   out[j++] = '0';
+        //   break;
+        // }
+        // for (len = 0; unum; unum >>= 4, ++len)
+        //   buffer[len] = "0123456789ABCDEF"[unum & 0xF]; //逆序的
 
-        for (int k = len - 1; k >= 0; --k)
-          out[j++] = buffer[k];
+        // for (int k = len - 1; k >= 0; --k)
+        //   out[j++] = buffer[k];
+        uitoa(unum, str, 10);
+        k = 0;
+        while (str[k] != '\0')
+        {
+          out[j++] = str[k++];
+        }
         break;
 
       default:
@@ -139,122 +169,24 @@ char *str;
   return j;
 }
 
-// itoa()函数把整数转换成字符串，并返回指向转换后的字符串的指针。
-
-
-char *uitoa(uint32_t value, char *str, int radix)
-{
-  char reverse[36];
-  char *p = reverse;
-
-  *p++ = '\0';
-  while (value != 0)
-  {
-    *p++ = "0123456789abcdef"[value % radix];
-    value /= radix;
-    if (value == 0)
-      break;
-  }
-  p--;
-
-  while (p >= reverse)
-  {
-    *str++ = *p--;
-  }
-
-  return str;
-}
-
-char *gcvt(double value, int ndigit, char *buf)
-{
-  char tmpbuf[72];
-  int int_part = (int)value;
-
-  double folat_part = value - int_part;
-
-  if (folat_part < 0)
-    folat_part = -folat_part;
-  itoa(int_part, tmpbuf, 10);
-
-  char *p = tmpbuf;
-
-  while (*p != '\0')
-    p++;
-
-  *p++ = '.';
-
-  while (ndigit > 0 && folat_part > 0.00000001)
-  {
-    *p++ = (int)(folat_part * 10) + '0';
-    folat_part = (folat_part * 10) - (int)(folat_part * 10);
-    ndigit--;
-  }
-
-  *p = '\0';
-  strcpy(buf, tmpbuf);
-  return buf;
-}
+// vsprintf return 要打印出来的字符串的长度
 int vsprintf(char *out, const char *fmt, va_list ap)
 {
   return vsnprintf(out, -1, fmt, ap);
-
-  // char *j;
-  // // char cha;
-  // va_list p_next_arg = ap;
-  // for (j = out; *fmt; fmt++)
-  // {
-  //   if (*fmt != '%')
-  //   {
-  //     *j++ = *fmt;
-  //     continue;
-  //   }
-  //   fmt++; // *fmt = '%'
-  //   switch (*fmt)
-  //   {
-  //   case 'd':
-  //     itoa(va_arg(p_next_arg, int), j, 10);
-  //     j += strlen(j);
-  //     break;
-  //   case 'x':
-  //     uitoa(va_arg(p_next_arg, unsigned int), j, 16);
-  //     j += strlen(j);
-  //     break;
-  //   case 'c':
-  //     *j++ = (char)va_arg(ap, int);
-  //     break;
-  //   case 's':
-  //     *j = '\0';
-  //     strcat(j, va_arg(p_next_arg, char *));
-  //     j += strlen(j);
-  //     break;
-  //   case 'f':
-  //     gcvt(va_arg(p_next_arg, double), 6, j);
-  //     j += strlen(j);
-  //     break;
-  //   default:
-  //     break;
-  //   }
-  // }
-  // *j = '\0';
-  // return strlen(j);
 }
 int printf(const char *fmt, ...)
 {
-  int i;
-  char buf[BUF_LEN];
-
+  char buffer[256];
   va_list arg;
   va_start(arg, fmt);
-  i = vsprintf(buf, fmt, arg);
-  // write(i,buf,  BUF_LEN);
-  // it can't be used
-  putstr(buf);
+
+  int i = vsprintf(buffer, fmt, arg);
+
+  putstr(buffer);
+
   va_end(arg);
   return i;
 }
-
-// vsprintf return 要打印出来的字符串的长度
-
 int sprintf(char *out, const char *fmt, ...)
 {
   va_list valist;
@@ -267,13 +199,7 @@ int sprintf(char *out, const char *fmt, ...)
 
 int snprintf(char *out, size_t n, const char *fmt, ...)
 {
-  va_list arg;
-  va_start(arg, fmt);
-
-  int done = vsnprintf(out, n, fmt, arg);
-
-  va_end(arg);
-  return done;
+  return 0;
 }
 
 #endif
