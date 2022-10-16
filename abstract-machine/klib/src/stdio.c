ca7fe781cf550c90e24451e72f1922fb9b4153a6
tracer-ics2022 <tracer@njuics.org>
2022-10-16 14:28:11 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:28:11 up  3:19,  1 user,  load average: 0.47, 0.42, 0.47

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,9 +2,11 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
+// #include <types.h>
+// libs
+// #include <string.h>
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-static char HEX_CHARACTERS[] = "0123456789ABCDEF";
-#define BIT_WIDE_HEX 8
 #define BUF_LEN 256
 #define append(x) \
   {               \
@@ -23,7 +25,7 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
   unsigned int unum;
   uint32_t pointer;
   bool state = true;
-  int i = 0, j = 0; //模仿一个状态机
+  int i = 0, j = 0;
   for (; fmt[i] != '\0'; ++i)
   {
     if (state)
@@ -56,11 +58,11 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
         if (num < 0)
         {
           append('-');
-          num = 0 - num;
+          num = -num;
         }
         for (len = 0; num; num /= 10, ++len)
-          buffer[len] = num % 10 + '0';//逆序的
-          // buffer[len] = HEX_CHARACTERS[num % 10]; //逆序的
+          buffer[len] = num % 10 + '0';
+        // buffer[len] = HEX_CHARACTERS[num % 10]; 
         for (int k = len - 1; k >= 0; --k)
           append(buffer[k]);
         break;
@@ -73,8 +75,8 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
       case 'p':
         pointer = va_arg(ap, uint32_t);
         for (len = 0; pointer; pointer /= 16, ++len)
-          buffer[len] = HEX_CHARACTERS[pointer % 16]; //逆序的
-        for (int k = 0; k < BIT_WIDE_HEX - len; ++k)
+          buffer[len] = "0123456789ABCDEF"[pointer % 16]; 
+        for (int k = 0; k < 8 - len; ++k)
           append('0');
 
         for (int k = len - 1; k >= 0; --k)
@@ -89,7 +91,7 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
           break;
         }
         for (len = 0; unum; unum >>= 4, ++len)
-          buffer[len] = HEX_CHARACTERS[unum & 0xF]; //逆序的
+          buffer[len] = "0123456789ABCDEF"[unum & 0xF]; //逆序的
 
         for (int k = len - 1; k >= 0; --k)
           append(buffer[k]);
@@ -105,9 +107,135 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
   out[j] = '\0';
   return j;
 }
+
+char *itoa(int value, char *str, int radix)
+{
+  char reverse[36];
+  char *p = reverse;
+  bool sign = (value >= 0) ? true : false;
+
+  value = (value >= 0) ? value : -value;
+  *p++ = '\0';
+  while (value >= 0)
+  {
+    *p++ = "0123456789abcdef"[value % radix];
+    value /= radix;
+    if (value == 0)
+      break;
+  }
+
+  if (!sign)
+  {
+    *p = '-';
+  }
+  else
+  {
+    p--;
+  }
+
+  while (p >= reverse)
+  {
+    *str++ = *p--;
+  }
+
+  return str;
+}
+
+char *uitoa(uint32_t value, char *str, int radix)
+{
+  char reverse[36];
+  char *p = reverse;
+
+  *p++ = '\0';
+  while (value != 0)
+  {
+    *p++ = "0123456789abcdef"[value % radix];
+    value /= radix;
+    if (value == 0)
+      break;
+  }
+  p--;
+
+  while (p >= reverse)
+  {
+    *str++ = *p--;
+  }
+
+  return str;
+}
+
+/* NOT WORK TODO */
+char *gcvt(double value, int ndigit, char *buf)
+{
+  char tmpbuf[72];
+  int int_part = (int)value;
+
+  double folat_part = value - int_part;
+
+  if (folat_part < 0)
+    folat_part = -folat_part;
+  itoa(int_part, tmpbuf, 10);
+
+  char *p = tmpbuf;
+
+  while (*p != '\0')
+    p++;
+
+  *p++ = '.';
+
+  while (ndigit > 0 && folat_part > 0.00000001)
+  {
+    *p++ = (int)(folat_part * 10) + '0';
+    folat_part = (folat_part * 10) - (int)(folat_part * 10);
+    ndigit--;
+  }
+
+  *p = '\0';
+  strcpy(buf, tmpbuf);
+  return buf;
+}
 int vsprintf(char *out, const char *fmt, va_list ap)
 {
   return vsnprintf(out, -1, fmt, ap);
+  // char *j;
+  // // char cha;
+  // va_list p_next_arg = ap;
+  // for (j = out; *fmt; fmt++)
+  // {
+  //   if (*fmt != '%')
+  //   {
+  //     *j++ = *fmt;
+  //     continue;
+  //   }
+  //   fmt++; // *fmt = '%'
+  //   switch (*fmt)
+  //   {
+  //   case 'd':
+  //     itoa(va_arg(p_next_arg, int), j, 10);
+  //     j += strlen(j);
+  //     break;
+  //   case 'x':
+  //     uitoa(va_arg(p_next_arg, unsigned int), j, 16);
+  //     j += strlen(j);
+  //     break;
+  //   case 'c':
+  //     *j++ = (char)va_arg(ap, int);
+  //     break;
+  //   case 's':
+  //     *j = '\0';
+  //     strcat(j, va_arg(p_next_arg, char *));
+  //     j += strlen(j);
+  //     break;
+  //   case 'f':
+  //     gcvt(va_arg(p_next_arg, double), 6, j);
+  //     j += strlen(j);
+  //     break;
+  //   default:
+  //     break;
+  //   }
+  // }
+  // *j = '\0';
+  // return strlen(j);
 }
 int printf(const char *fmt, ...)
 {
