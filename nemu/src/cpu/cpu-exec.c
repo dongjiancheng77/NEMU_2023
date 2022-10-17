f8347116ed6aa89fd5992ecc286db308b1765d3f
tracer-ics2022 <tracer@njuics.org>
2022-10-17 19:02:16 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:02:16 up 31 min,  3 users,  load average: 0.62, 0.34, 0.33

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -23,7 +23,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INST_TO_PRINT 100
+#define MAX_INST_TO_PRINT 32
 bool wp_state;
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
