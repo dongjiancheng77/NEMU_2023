9a99c0af8de21037fb3b2b7c725d3ecf85612910
tracer-ics2022 <tracer@njuics.org>
2022-10-18 21:14:48 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:14:48 up  2:21,  1 user,  load average: 0.53, 0.34, 0.36

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -177,7 +177,7 @@ static int parse_args(int argc, char *argv[])
       {0, 0, NULL, 0},
   };
   int o;
-  while ((o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1)
+  while ((o = getopt_long(argc, argv, "-bhl:e:d:p:", table, NULL)) != -1)
   {
     switch (o)
     {
