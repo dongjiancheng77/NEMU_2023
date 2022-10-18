3b60aaaf86428b1986db1f3735cf33c63cb87f9e
tracer-ics2022 <tracer@njuics.org>
2022-10-18 19:51:37 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:51:37 up 58 min,  1 user,  load average: 0.42, 0.39, 0.44

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -187,7 +187,12 @@ void cpu_exec(uint64_t n)
         (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) : (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
         nemu_state.halt_pc);
     // fall through
-
+#ifdef CONFIG_ITRACE
+    if (nemu_state.state != NEMU_STOP && nemu_state.state != NEMU_QUIT)
+    {
+      ringbuf_display();
+    }
+#endif
   case NEMU_QUIT:
     statistic();
   }
