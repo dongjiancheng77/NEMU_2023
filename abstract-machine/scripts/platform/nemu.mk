03cf2162f9978acfcb0b775463903d9a20822f31
tracer-ics2022 <tracer@njuics.org>
2022-10-18 21:29:48 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:29:48 up  2:36,  1 user,  load average: 1.02, 0.51, 0.40

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -12,7 +12,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -b 
-NEMUFLAGS += -e $(shell dirname $(IMAGE).elf)
+NEMUFLAGS += -e $(IMAGE).elf
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
