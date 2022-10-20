493918874de9ad5946abbef3ff7c27574e548369
tracer-ics2022 <tracer@njuics.org>
2022-10-20 23:05:36 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:05:36 up  1:20,  1 user,  load average: 0.69, 0.58, 0.60

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,50 +2,45 @@
 #include <nemu.h>
 #include <stdio.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-
+static int w;
+static int h;
 void __am_gpu_init()
 {
-  // // int i;
-  // const uint32_t vgainfo = inl(VGACTL_ADDR);
-  // w = vgainfo >> 16;
-  // h = (vgainfo << 16) >> 16;
-  // // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // // for (i = 0; i < w * h; i++)
-  // //   fb[i] = i;
-  // outl(SYNC_ADDR, 1);
+  int i;
+  const uint32_t vgainfo = inl(VGACTL_ADDR);
+  w = vgainfo >> 16;
+  h = (vgainfo << 16) >> 16;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++)
+    fb[i] = i;
+  outl(SYNC_ADDR, 1);
 
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
 {
-  uint32_t vgainfo = inl(VGACTL_ADDR);
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = (vgainfo >> 16) , .height = (vgainfo << 16) >> 16,
-    .vmemsz = 0
-  };
+
+  *cfg = (AM_GPU_CONFIG_T){
+      .present = true, .has_accel = false, .width = w, .height = h, .vmemsz = h * w};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
 {
-  int i, j, pi, pj;
-  uint32_t* pixels = ctl->pixels;
-  uint32_t wh_data = inl(VGACTL_ADDR);
-  uint32_t width = (wh_data >> 16) & 0xffff;
-  for (i = ctl->y, pi = 0; pi < ctl->h; ++ i, ++pi) {
-    for (j = ctl->x, pj = 0; pj < ctl->w; ++ j, ++pj) {
-      int fb_offset = (i * width + j) * 4;
-      int pixel_offset = pi* ctl->w + pj;
-      outl(FB_ADDR + fb_offset, *(pixels + pixel_offset));
-    }
-  }
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+const uint32_t* pixel = (uint32_t*)ctl->pixels;
+	uint32_t* base = (uint32_t*)(uintptr_t)FB_ADDR;
+
+	for (int i = 0; i < ctl->h; ++i)
+	{
+		for (int j = 0; j < ctl->w; ++j)
+		{
+			const uint32_t pOffset = i * ctl->w + j;
+			const uint32_t baseOffset = ((ctl->y + i) * w + (ctl->x + j));
+			base[baseOffset] = pixel[pOffset];
+		}
+	}
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status)
 {
   status->ready = true;
 }
-
