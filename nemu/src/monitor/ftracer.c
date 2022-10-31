2eec56ebfc81ba221e2c04c63c59eeab18abcf2e
tracer-ics2022 <tracer@njuics.org>
2022-10-31 20:16:26 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:16:26 up  1:51,  1 user,  load average: 0.64, 0.63, 0.78

--- nemu/src/monitor/ftracer.c
+++ nemu/src/monitor/ftracer.c
@@ -1,4 +1,4 @@
-// #ifdef CONFIG_FTRACE
+#ifdef CONFIG_FTRACE
 #include <isa.h>
 #include <elf.h>
 #include "ftracer.h"
@@ -8,16 +8,6 @@ functab_node *functab_head;
 /// @brief
 /// @param addr
 /// @return
-void node_init(char *name, word_t addr, word_t size)
-{
-  functab_node *newnode = (functab_node *)malloc(sizeof(functab_node));
-  newnode->addr = addr;
-  newnode->addr_end = addr + size;
-  newnode->name = (char *)malloc(strlen(name) + 1);
-  strcpy(newnode->name, name);
-  newnode->next = functab_head;
-  functab_head = newnode;
-}
 functab_node *functab_find(vaddr_t addr)
 {
   functab_node *ptr = functab_head;
@@ -31,6 +21,17 @@ functab_node *functab_find(vaddr_t addr)
   }
   return NULL;
 }
+void functab_push(char *name, word_t addr, word_t size)
+{
+  functab_node *newnode = (functab_node *)malloc(sizeof(functab_node));
+  newnode->addr = addr;
+  newnode->addr_end = addr + size;
+  newnode->name = (char *)malloc(strlen(name) + 1);
+  strcpy(newnode->name, name);
+  newnode->next = functab_head;
+  functab_head = newnode;
+}
+
 void load_elf(char *elf_file)
 {
   if (elf_file == NULL)
@@ -40,18 +41,19 @@ void load_elf(char *elf_file)
     return;
   }
   printf("====== Reading ELF File ======\n");
-  FILE *elf = fopen(elf_file, "rb");
-  fseek(elf, 0, SEEK_END);
-  long size = ftell(elf);
-    Elf32_Shdr *strtab_shdr = NULL;
+  FILE *fp = fopen(elf_file, "rb");
+  assert(elf != NULL);
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
   void *elf_buf = malloc(size);
-  fseek(elf, 0, SEEK_SET);
-  int succ = fread(elf_buf, size, 1, elf);
+  fseek(fp, 0, SEEK_SET);
+  int succ = fread(elf_buf, size, 1, fp);
   if (succ != 1)
   {
     panic("read elf failed!");
   }
-  fclose(elf);
+  fclose(fp);
+
   Elf32_Ehdr *elf_ehdr = elf_buf;
   Elf32_Shdr *symtab_shdr = NULL;
   for (int i = 0; i < elf_ehdr->e_phnum; ++i)
@@ -60,16 +62,19 @@ void load_elf(char *elf_file)
     Elf32_Phdr *elf_phdr = elf_buf + phdr_off;
     if (elf_phdr->p_type == PT_LOAD)
     {
-      void *seg_ptr = guest_to_host(elf_phdr->p_vaddr);
-      memcpy(seg_ptr, elf_buf + elf_phdr->p_offset, elf_phdr->p_filesz);
-      memset(seg_ptr + elf_phdr->p_filesz, 0, elf_phdr->p_memsz - elf_phdr->p_filesz);
+      void *segment_ptr = guest_to_host(elf_phdr->p_vaddr);
+      memcpy(segment_ptr, elf_buf + elf_phdr->p_offset, elf_phdr->p_filesz);
+      memset(segment_ptr + elf_phdr->p_filesz, 0, elf_phdr->p_memsz - elf_phdr->p_filesz);
     }
   }
+
   Elf32_Shdr *shstrtab_shdr = (elf_ehdr->e_shstrndx * elf_ehdr->e_shentsize + elf_ehdr->e_shoff) + elf_buf;
+  Elf32_Shdr *strtab_shdr = NULL;
   char *shstrtab_ptr = elf_buf + shstrtab_shdr->sh_offset;
   for (int i = 0; i < elf_ehdr->e_shnum; ++i)
   {
-    Elf32_Shdr *elf_shdr = elf_buf + i * elf_ehdr->e_shentsize + elf_ehdr->e_shoff;
+    int shdr_off = i * elf_ehdr->e_shentsize + elf_ehdr->e_shoff;
+    Elf32_Shdr *elf_shdr = elf_buf + shdr_off;
     switch (elf_shdr->sh_type)
     {
     case SHT_STRTAB:
@@ -81,15 +86,17 @@ void load_elf(char *elf_file)
     }
     break;
   }
-
-  char *strtab_ptr = elf_buf + strtab_shdr->sh_offset;
-  for (int i = 0; i < symtab_shdr->sh_size; i += symtab_shdr->sh_entsize)
+  if (symtab_shdr != NULL)
   {
-    Elf32_Sym *elf_sym = elf_buf + symtab_shdr->sh_offset + i;
-    if (ELF32_ST_TYPE(elf_sym->st_info) == STT_FUNC)
+    char *strtab_ptr = elf_buf + strtab_shdr->sh_offset;
+    for (int i = 0; i < symtab_shdr->sh_size; i += symtab_shdr->sh_entsize)
     {
-      printf("func-symbol: %s \t size:%d \t" FMT_WORD " - " FMT_WORD "\n", strtab_ptr + elf_sym->st_name, elf_sym->st_size, elf_sym->st_value, elf_sym->st_value + elf_sym->st_size);
-      node_init(strtab_ptr + elf_sym->st_name, elf_sym->st_value, elf_sym->st_size);
+      Elf32_Sym *elf_sym = elf_buf + symtab_shdr->sh_offset + i;
+      if (ELF32_ST_TYPE(elf_sym->st_info) == STT_FUNC)
+      {
+        printf("func-symbol: %s \t size:%d \t" FMT_WORD " - " FMT_WORD "\n", strtab_ptr + elf_sym->st_name, elf_sym->st_size, elf_sym->st_value, elf_sym->st_value + elf_sym->st_size);
+        functab_push(strtab_ptr + elf_sym->st_name, elf_sym->st_value, elf_sym->st_size);
+      }
     }
   }
 
@@ -97,4 +104,4 @@ void load_elf(char *elf_file)
   // one malloc one free
   return;
 }
-// #endif
+#endif
