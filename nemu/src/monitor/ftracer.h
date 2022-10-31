5693e14ecda1f220f79276ee7b624912c8216659
tracer-ics2022 <tracer@njuics.org>
2022-10-31 20:13:45 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:13:45 up  1:49,  1 user,  load average: 0.80, 0.63, 0.81

--- nemu/src/monitor/ftracer.h
+++ nemu/src/monitor/ftracer.h
@@ -10,7 +10,7 @@ typedef struct functab_node
     vaddr_t addr_end;
 } functab_node;
 extern functab_node* functab_head;
-void functab_push(char *name, word_t addr, word_t size);
+void node_init(char *name, word_t addr, word_t size);
 void load_elf(char* elf_file);
 functab_node *functab_find(vaddr_t addr);
 
