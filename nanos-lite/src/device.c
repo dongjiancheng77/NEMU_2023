ae6f8acc0bb3740fa4bbbae019106e53749c1932
tracer-ics2022 <tracer@njuics.org>
2022-11-22 22:59:38 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:59:38 up  2:40,  1 user,  load average: 0.62, 0.47, 0.52

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,66 +1,36 @@
 #include <common.h>
 
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
-#define MULTIPROGRAM_YIELD() yield()
+# define MULTIPROGRAM_YIELD() yield()
 #else
-#define MULTIPROGRAM_YIELD()
+# define MULTIPROGRAM_YIELD()
 #endif
 
 #define NAME(key) \
   [AM_KEY_##key] = #key,
 
-static AM_GPU_FBDRAW_T gpu_fbdraw;
-static AM_GPU_CONFIG_T gpu_config;
-
 static const char *keyname[256] __attribute__((used)) = {
-    [AM_KEY_NONE] = "NONE",
-    AM_KEYS(NAME)};
+  [AM_KEY_NONE] = "NONE",
+  AM_KEYS(NAME)
+};
 
-size_t serial_write(const void *buf, size_t offset, size_t len)
-{
+size_t serial_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
-size_t events_read(void *buf, size_t offset, size_t len)
-{
+size_t events_read(void *buf, size_t offset, size_t len) {
   return 0;
 }
 
-size_t dispinfo_read(void *buf, size_t offset, size_t len)
-{
+size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   return 0;
 }
 
-size_t fb_write(const void *buf, size_t offset, size_t len)
-{
-  // return 0;
-  if (len == 0)
-  {
-    // printf("end!\n");
-    gpu_fbdraw.sync = 1;
-    gpu_fbdraw.w = 0;
-    gpu_fbdraw.h = 0;
-    ioe_write(AM_GPU_FBDRAW, &gpu_fbdraw);
-    return 0;
-  }
-
-  // ioe_read(AM_GPU_CONFIG, &gpu_config);
-  int width = gpu_config.width;
-  // printf("screen is %d\n", width);
-
-  gpu_fbdraw.pixels = (void *)buf;
-  gpu_fbdraw.w = len;
-  gpu_fbdraw.h = 1;
-  gpu_fbdraw.x = offset % width;
-  gpu_fbdraw.y = offset / width;
-  // printf("w,h is %d,%d\n",gpu_fbdraw.w,gpu_fbdraw.h);
-  gpu_fbdraw.sync = 0;
-  ioe_write(AM_GPU_FBDRAW, &gpu_fbdraw);
-  return len;
+size_t fb_write(const void *buf, size_t offset, size_t len) {
+  return 0;
 }
 
-void init_device()
-{
+void init_device() {
   Log("Initializing devices...");
   ioe_init();
 }
