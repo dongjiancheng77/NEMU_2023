c20963fa91d152e3d1abca38bb3bf85a41007760
tracer-ics2022 <tracer@njuics.org>
2022-11-22 23:15:50 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:15:50 up  2:56,  1 user,  load average: 0.76, 0.54, 0.51

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,37 +1,129 @@
 #include <fs.h>
 
-typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
+typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
-typedef struct {
+typedef struct
+{
   char *name;
   size_t size;
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
-    size_t fs_offset;
+  size_t fs_offset;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum
+{
+  FD_STDIN,
+  FD_STDOUT,
+  FD_STDERR,
+  FD_FB
+};
 
-size_t invalid_read(void *buf, size_t offset, size_t len) {
+size_t invalid_read(void *buf, size_t offset, size_t len)
+{
   panic("should not reach here");
   return 0;
 }
 
-size_t invalid_write(const void *buf, size_t offset, size_t len) {
+size_t invalid_write(const void *buf, size_t offset, size_t len)
+{
   panic("should not reach here");
   return 0;
 }
-
+size_t fb_write(const void *buf, size_t offset, size_t len);
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write},
 #include "files.h"
 };
 
-void init_fs() {
+int fs_open(const char *pathname, int flags, int mode)
+{
+  int i;
+  for (i = 0; i < sizeof(file_table) / sizeof(Finfo); ++i)
+  {
+    if (file_table[i].name && strcmp(pathname, file_table[i].name) == 0)
+    {
+      file_table[i].fs_offset = file_table[i].disk_offset;
+      return i; // simply assign file_table index to fd
+    }
+  }
+  return -1;
+}
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+void init_fs()
+{
   // TODO: initialize the size of /dev/fb
+  AM_GPU_CONFIG_T fbctl = io_read(AM_GPU_CONFIG);
+  file_table[FD_FB].disk_offset = 0;
+  file_table[FD_FB].size = fbctl.width * fbctl.height * sizeof(uint32_t);
+}
+size_t fs_read(int fd, void *buf, size_t len)
+{
+  if (file_table[fd].read)
+  {
+    len = file_table[fd].read(buf, file_table[fd].fs_offset, len);
+    file_table[fd].fs_offset += len;
+    return len;
+  }
+  // fallback to ramdisk read
+  if (file_table[fd].fs_offset - file_table[fd].disk_offset + len > file_table[fd].size)
+  {
+    len = file_table[fd].size + file_table[fd].disk_offset - file_table[fd].fs_offset;
+  }
+  // Log("file_table[fd].fs_offset: %d, disk_off: %d, size: %d, len: %d", file_table[fd].fs_offset, file_table[fd].disk_offset,file_table[fd].size, len);
+  ramdisk_read(buf, file_table[fd].fs_offset, len);
+  file_table[fd].fs_offset += len;
+  return len;
+}
+
+size_t fs_write(int fd, void *buf, size_t len)
+{
+  if (file_table[fd].write)
+  {
+    len = file_table[fd].write(buf, file_table[fd].fs_offset, len);
+    file_table[fd].fs_offset += len;
+    return len;
+  }
+  if (file_table[fd].fs_offset - file_table[fd].disk_offset + len > file_table[fd].size)
+  {
+    len = file_table[fd].size + file_table[fd].disk_offset - file_table[fd].fs_offset;
+  }
+  ramdisk_write(buf, file_table[fd].fs_offset, len);
+  file_table[fd].fs_offset += len;
+  return len;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence)
+{
+  size_t new_offset;
+  switch (whence)
+  {
+  case SEEK_SET:
+    new_offset = file_table[fd].disk_offset + offset;
+    break;
+  case SEEK_CUR:
+    new_offset = file_table[fd].fs_offset + offset;
+    break;
+  case SEEK_END:
+    new_offset = file_table[fd].disk_offset + file_table[fd].size + offset;
+    break;
+  default:
+    return -1;
+  }
+  if (new_offset >= file_table[fd].disk_offset && new_offset <= file_table[fd].disk_offset + file_table[fd].size)
+  {
+    file_table[fd].fs_offset = new_offset;
+    return new_offset - file_table[fd].disk_offset;
+  }
+  else
+  {
+    return -1;
+  }
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,23 +9,40 @@
 #define Elf_Ehdr Elf32_Ehdr
 #define Elf_Phdr Elf32_Phdr
 #endif
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
-  // TODO();
-  Elf_Ehdr ehdr;
-
-  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*ehdr.e_phnum);
-  for (int i = 0; i < ehdr.e_phnum; ++i)
+  TODO();
+  Elf_Ehdr ehdr, *ptr_ehdr = &ehdr;
+  Elf_Phdr phdr, *ptr_phdr = &phdr;
+  uint32_t i, phoff;
+  int fd;
+  fd = fs_open(filename, 0, 0);
+  fs_read(fd, ptr_ehdr, sizeof(Elf_Ehdr));
+  for (i = 0; i < ehdr.e_phnum; ++i)
   {
-    if (phdr[i].p_type == PT_LOAD)
+    phoff = i * ehdr.e_phentsize + ehdr.e_phoff;
+    fs_lseek(fd, phoff, SEEK_SET);
+    fs_read(fd, ptr_phdr, sizeof(Elf_Phdr));
+    if (phdr.p_type == PT_LOAD)
     {
-      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-      memset((void *)phdr[i].p_vaddr + phdr[i].p_filesz, 0,
-             phdr[i].p_memsz - phdr[i].p_filesz);
+      uintptr_t vpage_start = phdr.p_vaddr & (~0xfff);
+      uintptr_t vpage_end = (phdr.p_vaddr + phdr.p_memsz - 1) & (~0xfff);
+      int page_num = ((vpage_end - vpage_start) >> 12) + 1;
+      uintptr_t page_ptr = (uintptr_t)new_page(page_num);
+      for (int j = 0; j < page_num; ++j)
+      {
+        map(&pcb->as,
+            (void *)(vpage_start + (j << 12)),
+            (void *)(page_ptr + (j << 12)),
+            MMAP_READ | MMAP_WRITE);
+      }
+      void *page_off = (void *)(phdr.p_vaddr & 0xfff);
+      fs_lseek(fd, phdr.p_offset, SEEK_SET);
+      fs_read(fd, page_ptr + page_off, phdr.p_filesz);
+      pcb->max_brk = vpage_end + PGSIZE;
+      // update max_brk, here it is the end of the last page
+      // this is related to heap, so ustack is not in consideration here
     }
   }
   return ehdr.e_entry;
