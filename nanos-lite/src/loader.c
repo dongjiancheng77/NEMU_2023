6a0186b146b6f692e4b4581cc9401d9953aa69a3
tracer-ics2022 <tracer@njuics.org>
2022-11-22 23:08:34 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:08:34 up  2:49,  1 user,  load average: 0.45, 0.47, 0.53

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,7 +18,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
 
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
   Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Ehdr));
+  ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*ehdr.e_phnum);
   for (int i = 0; i < ehdr.e_phnum; ++i)
   {
     if (phdr[i].p_type == PT_LOAD)
