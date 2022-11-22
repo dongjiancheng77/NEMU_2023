bcf570cdff77c3904bd697ad92ddf1a7df877665
tracer-ics2022 <tracer@njuics.org>
2022-11-22 22:58:13 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:58:13 up  2:39,  1 user,  load average: 0.51, 0.42, 0.51

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,31 +9,23 @@
 #define Elf_Ehdr Elf32_Ehdr
 #define Elf_Phdr Elf32_Phdr
 #endif
-
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
   TODO();
-  Elf_Ehdr ehdr, *ptr_ehdr = &ehdr;
-  Elf_Phdr phdr, *ptr_phdr = &phdr;
-  uint32_t i, phoff;
-  int fd;
-  fd = fs_open(filename, 0, 0);
-  fs_read(fd, ptr_ehdr, sizeof(Elf_Ehdr));
-  for (i = 0; i < ehdr.e_phnum; ++i)
+  Elf_Ehdr ehdr;
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Ehdr));
+  for (int i = 0; i < ehdr.e_phnum; ++i)
   {
-    phoff = i * ehdr.e_phentsize + ehdr.e_phoff;
-    fs_lseek(fd, phoff, SEEK_SET);
-    fs_read(fd, ptr_phdr, sizeof(Elf_Phdr));
-    if (phdr.p_type != PT_LOAD)
-      continue;
-    uintptr_t vpage_start = phdr.p_vaddr & (~0xfff);
-    uintptr_t vpage_end = (phdr.p_vaddr + phdr.p_memsz - 1) & (~0xfff);
-    int page_num = ((vpage_end - vpage_start) >> 12) + 1;
-    uintptr_t page_ptr = (uintptr_t)new_page(page_num);
-    void *page_off = (void *)(phdr.p_vaddr & 0xfff);
-    fs_lseek(fd, phdr.p_offset, SEEK_SET);
-    fs_read(fd, page_ptr + page_off, phdr.p_filesz);
-    pcb->max_brk = vpage_end + PGSIZE;
+    if (phdr[i].p_type == PT_LOAD)
+    {
+      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      memset((void *)phdr[i].p_vaddr + phdr[i].p_filesz, 0,
+             phdr[i].p_memsz-phdr[i].p_filesz);
+    }
   }
   return ehdr.e_entry;
   // return 0;
