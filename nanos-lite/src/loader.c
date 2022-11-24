73909429f20163704b25386c65ef36a637d59434
tracer-ics2022 <tracer@njuics.org>
2022-11-24 20:13:20 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:13:20 up  4:33,  1 user,  load average: 0.43, 0.47, 0.47

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,83 +11,83 @@
 #endif
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-// static uintptr_t loader(PCB *pcb, const char *filename)
-// {
-//   // TODO();
-//   Elf_Ehdr ehdr;
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+  // TODO();
+  Elf_Ehdr ehdr;
 
-//   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-//   // printf("Read 1\n");
-//   Elf_Phdr phdr[ehdr.e_phnum];
-//   ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*ehdr.e_phnum);
-//   for (int i = 0; i < ehdr.e_phnum; ++i)
-//   {
-//     if (phdr[i].p_type == PT_LOAD)
-//     {
-//       ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-//       memset((void *)phdr[i].p_vaddr + phdr[i].p_filesz, 0,
-//              phdr[i].p_memsz - phdr[i].p_filesz);
-//     }
-//   }
-//   return ehdr.e_entry;
-//   // return 0;
-// }
-
-
-#define min(x,y) x < y ? x : y
-
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-size_t get_ramdisk_size();
-
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  //TODO();
-	Elf32_Ehdr elf_header;
-	int success = ramdisk_read(&elf_header, 0, sizeof(Elf32_Ehdr));
-	assert(success);
-	assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
-	Elf32_Off phoff = elf_header.e_phoff;
-	for (int i = 0; i < elf_header.e_phnum; i ++) {
-		Elf32_Phdr p_header;
-		success = ramdisk_read(&p_header, phoff + i * sizeof(Elf32_Phdr), sizeof(Elf32_Phdr));
-		assert(success);
-		if (p_header.p_type == PT_LOAD) {
-			Elf32_Addr virtAddr = p_header.p_vaddr;
-			Elf32_Off segOffset = p_header.p_offset;
-			Elf32_Word fileSz = p_header.p_filesz;
-			Elf32_Word memSz = p_header.p_memsz;
-			
-			Elf32_Addr memPtr = virtAddr;
-			Elf32_Off filePtr = segOffset;
-			while (memPtr < virtAddr + fileSz) {
-				int readSize = min(sizeof(size_t), virtAddr + fileSz - memPtr);
-				if (readSize == sizeof(size_t)) {
-					size_t readData;
-					success = ramdisk_read(&readData, filePtr, readSize);
-					assert(success);
-					memcpy((void*)memPtr, &readData, readSize);
-					memPtr += readSize;
-					filePtr += readSize;
-				}
-				else {
-					for(int bt = 0; bt < readSize; bt ++) {
-						char readData;
-						success = ramdisk_read(&readData, filePtr, 1);
-						assert(success);
-						memcpy((void*)memPtr, &readData, 1);
-						filePtr ++;
-						memPtr ++;
-					}
-				}
-	//			printf("%d\n", virtAddr + fileSz - memPtr);
-			}
-			memPtr = virtAddr + fileSz;
-			memset((void*)memPtr, 0, memSz - fileSz);
-
-		}
-	}
-  return elf_header.e_entry;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  // printf("Read 1\n");
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  for (int i = 0; i < ehdr.e_phnum; ++i)
+  {
+    if (phdr[i].p_type == PT_LOAD)
+    {
+      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      memset((void *)phdr[i].p_vaddr + phdr[i].p_filesz, 0,
+             phdr[i].p_memsz - phdr[i].p_filesz);
+    }
+  }
+  return ehdr.e_entry;
+  // return 0;
 }
+
+
+// #define min(x,y) x < y ? x : y
+
+// size_t ramdisk_read(void *buf, size_t offset, size_t len);
+// size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+// size_t get_ramdisk_size();
+
+// static uintptr_t loader(PCB *pcb, const char *filename) {
+//   //TODO();
+// 	Elf32_Ehdr elf_header;
+// 	int success = ramdisk_read(&elf_header, 0, sizeof(Elf32_Ehdr));
+// 	assert(success);
+// 	assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
+// 	Elf32_Off phoff = elf_header.e_phoff;
+// 	for (int i = 0; i < elf_header.e_phnum; i ++) {
+// 		Elf32_Phdr p_header;
+// 		success = ramdisk_read(&p_header, phoff + i * sizeof(Elf32_Phdr), sizeof(Elf32_Phdr));
+// 		assert(success);
+// 		if (p_header.p_type == PT_LOAD) {
+// 			Elf32_Addr virtAddr = p_header.p_vaddr;
+// 			Elf32_Off segOffset = p_header.p_offset;
+// 			Elf32_Word fileSz = p_header.p_filesz;
+// 			Elf32_Word memSz = p_header.p_memsz;
+			
+// 			Elf32_Addr memPtr = virtAddr;
+// 			Elf32_Off filePtr = segOffset;
+// 			while (memPtr < virtAddr + fileSz) {
+// 				int readSize = min(sizeof(size_t), virtAddr + fileSz - memPtr);
+// 				if (readSize == sizeof(size_t)) {
+// 					size_t readData;
+// 					success = ramdisk_read(&readData, filePtr, readSize);
+// 					assert(success);
+// 					memcpy((void*)memPtr, &readData, readSize);
+// 					memPtr += readSize;
+// 					filePtr += readSize;
+// 				}
+// 				else {
+// 					for(int bt = 0; bt < readSize; bt ++) {
+// 						char readData;
+// 						success = ramdisk_read(&readData, filePtr, 1);
+// 						assert(success);
+// 						memcpy((void*)memPtr, &readData, 1);
+// 						filePtr ++;
+// 						memPtr ++;
+// 					}
+// 				}
+// 	//			printf("%d\n", virtAddr + fileSz - memPtr);
+// 			}
+// 			memPtr = virtAddr + fileSz;
+// 			memset((void*)memPtr, 0, memSz - fileSz);
+
+// 		}
+// 	}
+//   return elf_header.e_entry;
+// }
 
 void naive_uload(PCB *pcb, const char *filename)
 {
