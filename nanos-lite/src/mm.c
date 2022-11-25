3841efecefe7e6b8c29782bfa2b17f3ae5d05a50
tracer-ics2022 <tracer@njuics.org>
2022-11-25 14:08:31 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:08:31 up 26 min,  1 user,  load average: 0.64, 0.45, 0.38

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -6,9 +6,13 @@ static void *pf = NULL;
 
 void *new_page(size_t nr_page)
 {
-  return NULL;
-  //   pf += nr_page * 4 * 1024;
-  // return pf - nr_page * 4 * 1024;
+  // pf has been inited in init_mm
+  void *old_pf = pf;
+  memset(pf, 0, nr_page * PGSIZE);
+  pf += nr_page * PGSIZE;
+  assert(pf < (void *)heap.end);
+  // Log("new page allocated in %p", old);
+  return old_pf;
 }
 
 #ifdef HAS_VME
