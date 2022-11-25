367b15ba1696a50314e9fd726fd1f9b655abf728
tracer-ics2022 <tracer@njuics.org>
2022-11-25 14:49:30 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:49:30 up  1:07,  1 user,  load average: 0.25, 0.43, 0.46

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -56,7 +56,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
       memset((void *)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
-  printf("entry: %x\n", elf.e_entry);
+  // printf("entry: %x\n", elf.e_entry);
   return elf.e_entry;
 }
 
