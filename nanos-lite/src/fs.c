3005409c0d03ca45b7accfb97f77659b7fcbefd4
tracer-ics2022 <tracer@njuics.org>
2022-11-25 14:54:33 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:54:33 up  1:12,  1 user,  load average: 1.71, 0.73, 0.54

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -26,13 +26,13 @@ enum
 
 size_t invalid_read(void *buf, size_t offset, size_t len)
 {
-  panic("should not reach here");
+  // panic("should not reach here");
   return 0;
 }
 
 size_t invalid_write(const void *buf, size_t offset, size_t len)
 {
-  // panic("should not reach here");
+  panic("should not reach here");
   return 0;
 }
 size_t fb_write(const void *buf, size_t offset, size_t len);
