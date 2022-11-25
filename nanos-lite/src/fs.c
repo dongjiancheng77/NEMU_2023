e10624ae61c95c617ccf0502cb8b26d2d112e8b2
tracer-ics2022 <tracer@njuics.org>
2022-11-25 17:22:22 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:22:22 up  3:40,  1 user,  load average: 0.89, 0.46, 0.34

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -93,16 +93,16 @@ size_t fs_read(int fd, void *buf, size_t len)
 
 size_t fs_write(int fd, void *buf, size_t len)
 {
-  // if (file_table[fd].write)
-  // {
-  //   len = file_table[fd].write(buf, file_table[fd].fs_offset, len);
-  //   file_table[fd].fs_offset += len;
-  //   return len;
-  // }
-  // if (file_table[fd].fs_offset - file_table[fd].disk_offset + len > file_table[fd].size)
-  // {
-  //   len = file_table[fd].size + file_table[fd].disk_offset - file_table[fd].fs_offset;
-  // }
+  if (file_table[fd].write)
+  {
+    len = file_table[fd].write(buf, file_table[fd].fs_offset, len);
+    file_table[fd].fs_offset += len;
+    return len;
+  }
+  if (file_table[fd].fs_offset - file_table[fd].disk_offset + len > file_table[fd].size)
+  {
+    len = file_table[fd].size + file_table[fd].disk_offset - file_table[fd].fs_offset;
+  }
   ramdisk_write(buf, file_table[fd].fs_offset, len);
   file_table[fd].fs_offset += len;
   return len;
