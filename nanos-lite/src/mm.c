7b01d7169c39c06f9fd1d38c1a53633717f2249d
tracer-ics2022 <tracer@njuics.org>
2022-11-25 19:48:25 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:48:25 up 24 min,  1 user,  load average: 1.45, 0.91, 0.56

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -6,23 +6,25 @@ static void *pf = NULL;
 
 void *new_page(size_t nr_page)
 {
-  // pf has been inited in init_mm
-  void *old_pf = pf;
-  memset(pf, 0, nr_page * PGSIZE);
-  pf += nr_page * PGSIZE;
-  assert(pf < (void *)heap.end);
-  Log("new page allocated in %p", old_pf);
-  return old_pf;
+  // // pf has been inited in init_mm
+  // void *old_pf = pf;
+  // memset(pf, 0, nr_page * PGSIZE);
+  // pf += nr_page * PGSIZE;
+  // assert(pf < (void *)heap.end);
+  // Log("new page allocated in %p", old_pf);
+  // return old_pf;
+    return NULL;
 }
 
 #ifdef HAS_VME
 static void *pg_alloc(int n)
 {
-  // return NULL;
-    assert(n % (4096) == 0);
-  void *ret = new_page(n / (4096));
-  memset(ret, 0, n);
-  return ret;
+  // // return NULL;
+  //   assert(n % (4096) == 0);
+  // void *ret = new_page(n / (4096));
+  // memset(ret, 0, n);
+  // return ret;
+    return NULL;
 }
 #endif
 
@@ -34,25 +36,25 @@ extern PCB *current;
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk)
 {
-  uintptr_t max_page_end = current->max_brk;
-  // uintptr_t max_page_pn = (max_page_end >> 12) - 1;
-  uintptr_t brk_pn = brk >> 12; // 12
+  // uintptr_t max_page_end = current->max_brk;
+  // // uintptr_t max_page_pn = (max_page_end >> 12) - 1;
+  // uintptr_t brk_pn = brk >> 12; // 12
 
-  //[page_start, page_end) 所以，应该是大于等于就重新分配
-  // printf("申请内存 max_page_end: %p\t to brk: %p\n", max_page_end, brk);//分配从[max_page_pn, brk_pn]的所有页
-  if (brk >= max_page_end)
-  {
-    // void *allocted_page = new_page(brk_pn - max_page_pn + 1); //需要申请的新的页数
-    // for (int i = 0; i < brk_pn - max_page_pn + 1; ++i)
-    // {
-    //   // TODO: prot 有问题
-    //   map(&current->as, (void *)(max_page_end + i * 0xfff),
-    //       (void *)(allocted_page + i * 0xfff), 1);
-    // }
+  // //[page_start, page_end) 所以，应该是大于等于就重新分配
+  // // printf("申请内存 max_page_end: %p\t to brk: %p\n", max_page_end, brk);//分配从[max_page_pn, brk_pn]的所有页
+  // if (brk >= max_page_end)
+  // {
+  //   // void *allocted_page = new_page(brk_pn - max_page_pn + 1); //需要申请的新的页数
+  //   // for (int i = 0; i < brk_pn - max_page_pn + 1; ++i)
+  //   // {
+  //   //   // TODO: prot 有问题
+  //   //   map(&current->as, (void *)(max_page_end + i * 0xfff),
+  //   //       (void *)(allocted_page + i * 0xfff), 1);
+  //   // }
 
-    current->max_brk = (brk_pn + 1) << 12;
-    assert(current->max_brk > brk);
-  }
+  //   current->max_brk = (brk_pn + 1) << 12;
+  //   assert(current->max_brk > brk);
+  // }
   return 0;
 }
 
