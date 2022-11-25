cc6eba8b16d910e684a42cd2e5a7e281a9763a73
tracer-ics2022 <tracer@njuics.org>
2022-11-25 12:03:07 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:03:07 up  1:20,  1 user,  load average: 0.64, 0.56, 0.55

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -70,27 +70,28 @@ int _write(int fd, void *buf, size_t count)
 {
   return _syscall_((intptr_t)SYS_write, (intptr_t)fd, (intptr_t)buf, count);
 }
-extern char end;
-void *program_break = NULL;
+// extern char end;
+// void *program_break = NULL;
 void *_sbrk(intptr_t increment)
 {
-  if (program_break == NULL)
-  { // 初始化
-    program_break = &end;
-  }
-  void *old_program_break = program_break;
+  // if (program_break == NULL)
+  // { // 初始化
+  //   program_break = &end;
+  // }
+  // void *old_program_break = program_break;
 
-  int ret = _syscall_(SYS_brk, (intptr_t)(program_break + increment), 0, 0);
-  if (ret == 0)
-  {
-    program_break = program_break + increment;
-  }
-  else
-  {
-    assert(0);
-  }
+  // int ret = _syscall_(SYS_brk, (intptr_t)(program_break + increment), 0, 0);
+  // if (ret == 0)
+  // {
+  //   program_break = program_break + increment;
+  // }
+  // else
+  // {
+  //   assert(0);
+  // }
 
-  return old_program_break;
+  // return old_program_break;
+  return 0;
 }
 
 int _read(int fd, void *buf, size_t count)
