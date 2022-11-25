fc7dcab3da649e998c19465c23388bce7d5e2c8a
tracer-ics2022 <tracer@njuics.org>
2022-11-25 17:27:07 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:27:07 up  3:44,  1 user,  load average: 0.88, 0.68, 0.47

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,21 +2,21 @@
 #include <assert.h>
 
 int main() {
-  printf("%")
+  printf("1");
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
-
+  printf("2");
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-
+  printf("4");
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("1");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
