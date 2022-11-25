1d82be9c4864d39664bad71edde0370ef6803785
tracer-ics2022 <tracer@njuics.org>
2022-11-25 15:17:36 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:17:36 up  1:35,  1 user,  load average: 1.12, 0.59, 0.50

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -19,9 +19,9 @@ enum
   FD_STDIN,
   FD_STDOUT,
   FD_STDERR,
-  // FD_FB,
-  // FD_EVENTS,
-  // FD_DISPINFO
+  FD_FB,
+  FD_EVENTS,
+  FD_DISPINFO
 };
 
 size_t invalid_read(void *buf, size_t offset, size_t len)
@@ -74,17 +74,17 @@ int fs_open(const char *pathname, int flags, int mode)
 
 size_t fs_read(int fd, void *buf, size_t len)
 {
-  if (file_table[fd].read)
-  {
-    len = file_table[fd].read(buf, file_table[fd].fs_offset, len);
-    file_table[fd].fs_offset += len;
-    return len;
-  }
-  // fallback to ramdisk read
-  if (file_table[fd].fs_offset - file_table[fd].disk_offset + len > file_table[fd].size)
-  {
-    len = file_table[fd].size + file_table[fd].disk_offset - file_table[fd].fs_offset;
-  }
+  // if (file_table[fd].read)
+  // {
+  //   len = file_table[fd].read(buf, file_table[fd].fs_offset, len);
+  //   file_table[fd].fs_offset += len;
+  //   return len;
+  // }
+  // // fallback to ramdisk read
+  // if (file_table[fd].fs_offset - file_table[fd].disk_offset + len > file_table[fd].size)
+  // {
+  //   len = file_table[fd].size + file_table[fd].disk_offset - file_table[fd].fs_offset;
+  // }
   // Log("file_table[fd].fs_offset: %d, disk_off: %d, size: %d, len: %d", file_table[fd].fs_offset, file_table[fd].disk_offset,file_table[fd].size, len);
   ramdisk_read(buf, file_table[fd].fs_offset, len);
   file_table[fd].fs_offset += len;
@@ -93,16 +93,16 @@ size_t fs_read(int fd, void *buf, size_t len)
 
 size_t fs_write(int fd, void *buf, size_t len)
 {
-  if (file_table[fd].write)
-  {
-    len = file_table[fd].write(buf, file_table[fd].fs_offset, len);
-    file_table[fd].fs_offset += len;
-    return len;
-  }
-  if (file_table[fd].fs_offset - file_table[fd].disk_offset + len > file_table[fd].size)
-  {
-    len = file_table[fd].size + file_table[fd].disk_offset - file_table[fd].fs_offset;
-  }
+  // if (file_table[fd].write)
+  // {
+  //   len = file_table[fd].write(buf, file_table[fd].fs_offset, len);
+  //   file_table[fd].fs_offset += len;
+  //   return len;
+  // }
+  // if (file_table[fd].fs_offset - file_table[fd].disk_offset + len > file_table[fd].size)
+  // {
+  //   len = file_table[fd].size + file_table[fd].disk_offset - file_table[fd].fs_offset;
+  // }
   ramdisk_write(buf, file_table[fd].fs_offset, len);
   file_table[fd].fs_offset += len;
   return len;
