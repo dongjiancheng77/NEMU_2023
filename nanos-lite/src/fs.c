f3527171ebf0c48c9970cd4e2053ee4579d4d006
tracer-ics2022 <tracer@njuics.org>
2022-11-28 19:27:26 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:27:26 up 43 min,  1 user,  load average: 1.00, 0.61, 0.50

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -39,11 +39,11 @@ size_t fb_write(const void *buf, size_t offset, size_t len);
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
     [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
-    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
-    // [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write},
-    // [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write},
-    // [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
+    [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write},
+    [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write},
+    [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
