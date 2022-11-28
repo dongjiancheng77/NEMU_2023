783a7f7f49798735ae8d8a201982bd2caf1a73b9
tracer-ics2022 <tracer@njuics.org>
2022-11-28 19:24:17 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:24:17 up 40 min,  1 user,  load average: 0.37, 0.45, 0.45

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -42,8 +42,8 @@ static Finfo file_table[] __attribute__((used)) = {
     [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
     [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
     [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write},
-    [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write},
-    [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
+    // [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write},
+    // [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
