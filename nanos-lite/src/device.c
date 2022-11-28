2ea6a56f4c7df5851330ebfc754a132cee88873b
tracer-ics2022 <tracer@njuics.org>
2022-11-28 21:52:27 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:52:27 up  3:08,  1 user,  load average: 0.55, 0.58, 0.62

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -32,6 +32,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len)
 size_t events_read(void *buf, size_t offset, size_t len)
 {
   MULTIPROGRAM_YIELD();
+  printf("!");
   AM_INPUT_KEYBRD_T kbd_in = io_read(AM_INPUT_KEYBRD);
   if (kbd_in.keycode == AM_KEY_NONE)
     return 0;
