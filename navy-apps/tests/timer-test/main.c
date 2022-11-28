b03f999359dc45e68a8c721ce02e31864e68bdc4
tracer-ics2022 <tracer@njuics.org>
2022-11-28 19:23:45 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:23:45 up 39 min,  1 user,  load average: 0.43, 0.48, 0.45

---
+++ navy-apps/tests/timer-test/main.c
@@ -0,0 +1,23 @@
+#include <sys/time.h>
+#include <stdio.h>
+int main()
+{
+  int count = 0;
+  struct timeval old_tv, cur_tv;
+  struct timezone tz;
+  unsigned long old_usec, cur_usec;
+  gettimeofday(&old_tv, &tz);
+  old_usec = old_tv.tv_sec * 1000000 + old_tv.tv_usec;
+  while (count < 10)
+  {
+    gettimeofday(&cur_tv, &tz);
+    unsigned long cur_usec = cur_tv.tv_sec * 1000000 + cur_tv.tv_usec;
+    if (cur_usec - old_usec > 500000)
+    {
+      old_usec = cur_usec;
+      printf("tick %d\n", count);
+      count++;
+    }
+  }
+  return 0;
+}
