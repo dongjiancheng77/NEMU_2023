033cb520f451bae4ed555b9e48e91f9b6371b1b6
tracer-ics2022 <tracer@njuics.org>
2022-11-28 20:10:32 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:10:32 up  1:26,  1 user,  load average: 0.53, 0.73, 0.81

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -3,13 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
-  return 0;
+  // return 0;
+    static struct timeval timeval;
+  static struct timezone timezone;
+  int ret = gettimeofday(&timeval, &timezone);
+  return timeval.tv_usec / 1000;
 }
 
 int NDL_PollEvent(char *buf, int len) {
--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,25 +1,38 @@
 #include <sys/time.h>
 #include <stdio.h>
+#include <NDL.h>
 int main()
 {
   int count = 0;
-  struct timeval old_tv, cur_tv;
-  struct timezone tz;
-  unsigned long old_usec, cur_usec;
-  gettimeofday(&old_tv, &tz);
-  old_usec = old_tv.tv_sec * 1000000 + old_tv.tv_usec;
-  while (count < 10)
+  // struct timeval old_tv, cur_tv;
+  // struct timezone tz;
+  // unsigned long old_usec, cur_usec;
+  // gettimeofday(&old_tv, &tz);
+  // old_usec = old_tv.tv_sec * 1000000 + old_tv.tv_usec;
+  // uint32_t sec = 0;
+  // uint32_t usec = 0;
+  NDL_Init(0);
+  unsigned int old_ms = NDL_GetTicks();
+  unsigned int cur_ms = 0;
+  while (count < 32)
   {
 
-    gettimeofday(&cur_tv, &tz);
-    unsigned long cur_usec = cur_tv.tv_sec * 1000000 + cur_tv.tv_usec;
-    // printf("tick0 %ld\n", cur_usec);
-    if (cur_usec - old_usec > 500000)
+    // gettimeofday(&cur_tv, &tz);
+    // unsigned long cur_usec = cur_tv.tv_sec * 1000000 + cur_tv.tv_usec;
+    // // printf("tick0 %ld\n", cur_usec);
+    // if (cur_usec - old_usec > 500000)
+    // {
+    //   old_usec = cur_usec;
+    //   printf("tick %d\n", count);
+    //   count++;
+    //   printf("tick1 %d\n", count);
+    // }
+    cur_ms = NDL_GetTicks();
+    if (cur_ms - old_ms > 500)
     {
-      old_usec = cur_usec;
-      printf("tick %d\n", count);
+      old_ms = cur_ms;
+      printf("ndl tick %d\n", count);
       count++;
-      printf("tick1 %d\n", count);
     }
   }
   return 0;
