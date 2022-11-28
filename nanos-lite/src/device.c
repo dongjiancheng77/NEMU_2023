e8809c24c90fbc161b5dab6c5acd22d16b7f8110
tracer-ics2022 <tracer@njuics.org>
2022-11-28 22:13:37 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:13:37 up  3:29,  1 user,  load average: 1.46, 0.96, 0.77

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -49,8 +49,7 @@ size_t events_read(void *buf, size_t offset, size_t len)
     switch_program_index(3);
     return 0;
   }
-snprintf(buf, len, "%s %s\n", kbd_in.keydown ? "kd" : "ku", keyname[kbd_in.keycode]);
-  // printf("%s %d %s %d\n", (char*)buf, len, keyname[kbd_in.keycode], kbd_in.keydown);
+  // snprintf(buf, len, "%s %s\n", kbd_in.keydown ? "kd" : "ku", keyname[kbd_in.keycode]);
   return 1;
 }
 
