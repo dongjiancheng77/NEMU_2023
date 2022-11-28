666efb40e6b3885beb000532376bc9026c9a7be0
tracer-ics2022 <tracer@njuics.org>
2022-11-28 19:45:34 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:45:34 up  1:01,  1 user,  load average: 1.40, 1.23, 0.95

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -10,7 +10,7 @@ int main()
   old_usec = old_tv.tv_sec * 1000000 + old_tv.tv_usec;
   while (count < 10)
   {
-    // printf("tick0 %d\n", count);
+    printf("tick0 %d\n", cur_usec);
     gettimeofday(&cur_tv, &tz);
     unsigned long cur_usec = cur_tv.tv_sec * 1000000 + cur_tv.tv_usec;
     if (cur_usec - old_usec > 500000)
