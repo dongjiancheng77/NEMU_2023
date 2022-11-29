f3f1860b4cb5c47c278b32534fe3dcccbc619302
tracer-ics2022 <tracer@njuics.org>
2022-11-29 14:22:35 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:22:35 up  3:54,  1 user,  load average: 1.29, 1.02, 0.83

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -82,7 +82,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
   y += (screen_h - canvas_h) / 2;
   int fd = open("/dev/fb", O_WRONLY);
   assert(fd != -1);
-  printf("drawing to %d, %08X: %d %d %d %d\n", fd, *pixels, x, y, w, h);
+  printf("drawing to %d, %08X: %d %d %d %d", fd, *pixels, x, y, w, h);
   size_t base_offset = (y * screen_w + x) * sizeof(uint32_t);
   size_t pixel_offset = 0;
   int j, ret_seek=0, ret_write;
