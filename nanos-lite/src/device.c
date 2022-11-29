bbd1363801c2321a6b563c05ccf839cbc62523ec
tracer-ics2022 <tracer@njuics.org>
2022-11-29 15:08:13 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:08:13 up  4:40,  1 user,  load average: 0.38, 0.36, 0.47

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -64,8 +64,8 @@ size_t fb_write(const void *buf, size_t offset, size_t len)
   uintptr_t *ptr;
   ptr = (uintptr_t *)(&buf);
 
-  io_write(AM_GPU_MEMCPY, offset, (void *)*ptr, len);
-  io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
+
+  io_write(AM_GPU_FBDRAW, (offset/4)%400,(offset/4)/400, (void *)*ptr, len/4, 1, true);
   
   return len;
 }
