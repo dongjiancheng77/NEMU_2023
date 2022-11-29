4441caedf119be22bd572bef3f6b81ff908e6855
tracer-ics2022 <tracer@njuics.org>
2022-11-29 14:16:28 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:16:28 up  3:48,  1 user,  load average: 0.96, 0.94, 0.75

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -61,19 +61,13 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len)
 
 size_t fb_write(const void *buf, size_t offset, size_t len)
 {
-  MULTIPROGRAM_YIELD();
-  AM_GPU_CONFIG_T fbctl = io_read(AM_GPU_CONFIG);
-  size_t max_offset = fbctl.height * fbctl.width * sizeof(uint32_t);
-  if (offset + len > max_offset)
-  {
-    len = max_offset - offset;
-  }
-  AM_GPU_MEMCPY_T gpu_memcpy;
-  gpu_memcpy.dest = offset;
-  gpu_memcpy.size = len;
-  // gpu_memcpy.src = buf;
-  ioe_write(AM_GPU_MEMCPY, &gpu_memcpy);
-  return len; // len counts in BYTE!!!
+  uintptr_t *ptr;
+  ptr = (uintptr_t *)(&buf);
+
+  io_write(AM_GPU_MEMCPY, offset, (void *)*ptr, len);
+  io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
+  
+  return len;
 }
 
 void init_device()
