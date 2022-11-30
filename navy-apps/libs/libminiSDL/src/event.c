8aee84c6fd57a6b5718133206c49b366277c3ec8
tracer-ics2022 <tracer@njuics.org>
2022-11-30 11:32:16 +0800

    >  compile NEMU 211300060 董建承 Linux khons-Lenovo 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:32:16 up 55 min,  1 user,  load average: 0.39, 0.41, 0.57

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -9,10 +9,62 @@ int SDL_PushEvent(SDL_Event *ev)
 {
   return 0;
 }
+static char *key_action, *key_key;
+static uint8_t key_buf[sizeof(keyname) / sizeof(char *)] = {0};
+static int inline read_keyinfo(uint8_t *type, uint8_t *sym){
+  char key[64];
+  int ret = NDL_PollEvent(key, sizeof(key));
+  if (!ret){
+    return 0;
+  }
+  //printf("%s\n", key_buf);
+  key_action = key;
+  int i;
+  for (i = 0; key[i] != ' '; i++){}
+  key[i] = '\0';
+  key_key = &key[i + 1]; 
+  
+  //截断\n
+  for (i = 0;  key_key[i] != '\0' && key_key[i] != '\n'; i++){}
+  if (key_key[i] == '\n'){
+    key_key[i] = '\0';
+  }
+  
+  //strcmp("kd", key_action) == 0
+  if (key_action[1] == 'd'){//加速！！
+    *type = SDL_KEYDOWN;
+  }else{
+    *type = SDL_KEYUP;
+  }
 
+  for (i = 0; i < sizeof(keyname) / sizeof(char *); ++i){
+    //剪枝掉很多
+    if (key_key[0] == keyname[i][0] && strcmp(key_key, keyname[i]) == 0){
+      *sym = i;
+      //printf("%d %d\n", *type, *sym);
+      return ret;
+    }
+  }
+}
 int SDL_PollEvent(SDL_Event *ev)
 {
-  return 0;
+  uint8_t type = 0, sym = 0;
+  if (read_keyinfo(&type, &sym))
+  {
+    ev->type = type;
+    ev->key.keysym.sym = sym;
+
+    if (ev->type == SDL_KEYDOWN)
+      key_buf[sym] = 1;
+    else if (ev->type == SDL_KEYUP)
+      key_buf[sym] = 0;
+  }
+  else
+  {
+    return 0;
+  }
+
+  return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event)
@@ -21,7 +73,7 @@ int SDL_WaitEvent(SDL_Event *event)
   // printf("1");
   while (NDL_PollEvent(buf, 32) == 0)
     ;
-  printf("2 %s \n", buf);
+  // printf("2 %s \n", buf);
   if (strncmp(buf, "kd", 2) == 0)
   {
     event->type = SDL_KEYDOWN;
@@ -38,6 +90,10 @@ int SDL_WaitEvent(SDL_Event *event)
     if (((strlen(buf + 3) - 1) == strlen(keyname[i])) && (strncmp(buf + 3, keyname[i], strlen(keyname[i])) == 0))
     {
       event->key.keysym.sym = i;
+      if (event->type == SDL_KEYDOWN)
+        key_buf[i] = 1;
+      else if (event->type == SDL_KEYUP)
+        key_buf[i] = 0;
       break;
     }
   }
